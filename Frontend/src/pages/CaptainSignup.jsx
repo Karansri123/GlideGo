19133607b5dea2d35bda64b8e4ee1b34e791b027
Karansri123 <karansrivastava189@gmailcom>
2024-12-16 22:11:23 +0530

    login and signup page done

--- Frontend/src/pages/CaptainSignup.jsx
+++ Frontend/src/pages/CaptainSignup.jsx
@@ -1,9 +1,108 @@
+/* eslint-disable react/no-unescaped-entities */
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const CaptainSignup = () => {
+  const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [firstName,setFirstName] = useState("");
+    const [lastName,setLastName] = useState("");
+      // eslint-disable-next-line no-unused-vars
+      const [userData , setUserData] = useState({});
+  
+    const submitHandler = (e) => {
+      e.preventDefault();
+      setUserData({
+        fullName:{
+          firstName:firstName,
+          lastName:lastName,
+        },
+        email: email,
+        password: password,
+      })
+      setEmail('');
+      setPassword('');
+      setFirstName('');
+      setLastName('');
+    };
   return (
-    <div>CaptainSignup</div>
+    <div>
+    <div className="py-5 px-5 h-screen flex flex-col justify-between">
+      <div>
+        <img
+          className="w-16 mb-5 mix-blend-multiply"
+          src="https://www.logosvgpng.com/wp-content/uploads/2021/12/glide-apps-logo-vector.png"
+          alt=""
+        />
+        <form
+          onSubmit={(e) => {
+            submitHandler(e);
+          }}
+        >
+          <h3 className="text-lg w-full font-medium mb-2">What's our Captain's name</h3>
+          <div className="flex gap-4 mb-6">
+            <input
+              required
+              className="bg-[#eeeee] w-1/2 rounded px-4 py-2 border  text-lg placeholder:text-base"
+              type="text"
+              placeholder="First name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            <input
+              required
+              className="bg-[#eeeee] w-1/2  rounded px-4 py-2 border  text-lg placeholder:text-base"
+              type="text"
+              placeholder="Last name"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+          <h3 className="text-lg font-medium mb-2">What's our Captain's email</h3>
+          <input
+            required
+            className="bg-[#eeeee] mb-6 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+            type="email"
+            placeholder="email@example.com"
+            value={email}
+            onChange={(e)=>{
+              setEmail(e.target.value)
+            }}
+          />
+
+          <h3 className="text-lg font-medium mb-2">Enter Password</h3>
+          <input
+            required
+            className="bg-[#eeeee] mb-6 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e)=>{
+              setPassword(e.target.value)
+            }}
+          />
+
+          <button className="bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2  w-full text-lg placeholder:text-base">
+            Login
+          </button>
+        </form>
+        <p className="text-center">
+          Already have a account?{" "}
+          <Link to="/captain-login" className="text-blue-600">
+            Login here
+          </Link>
+        </p>
+      </div>
+      <div>
+        <p className="text-[10px] leading-tight">
+          This site is protected by reCAPTCHA and the{" "}
+          <span className="underline">Google Privacy Policy</span> and{" "}
+          <span className="underline">Terms of Service apply</span>.
+        </p>
+      </div>
+    </div>
+  </div>
   )
 }
 
--- Frontend/src/pages/UserSignup.jsx
+++ Frontend/src/pages/UserSignup.jsx
@@ -6,17 +6,25 @@ import { Link } from "react-router-dom";
 const UserSignup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  // eslint-disable-next-line no-unused-vars
-  const [userData, setUserData] = useState({});
+  const [firstName,setFirstName] = useState("");
+  const [lastName,setLastName] = useState("");
+    // eslint-disable-next-line no-unused-vars
+    const [userData , setUserData] = useState({});
 
   const submitHandler = (e) => {
     e.preventDefault();
     setUserData({
+      fullName:{
+        firstName:firstName,
+        lastName:lastName,
+      },
       email: email,
       password: password,
-    });
-    setEmail("");
-    setPassword("");
+    })
+    setEmail('');
+    setPassword('');
+    setFirstName('');
+    setLastName('');
   };
 
   return (
@@ -33,38 +41,50 @@ const UserSignup = () => {
               submitHandler(e);
             }}
           >
-            <h3 className="text-base font-medium mb-2">What's your name</h3>
+            <h3 className="text-lg font-medium mb-2">What's your name</h3>
             <div className="flex gap-4 mb-6">
               <input
                 required
-                className="bg-[#eeeee] w-1/2 rounded px-4 py-2 border  text-base placeholder:text-sm"
+                className="bg-[#eeeee] w-1/2 rounded px-4 py-2 border  text-lg placeholder:text-base"
                 type="text"
                 placeholder="First name"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
               />
               <input
                 required
-                className="bg-[#eeeee] w-1/2  rounded px-4 py-2 border  text-base placeholder:text-sm"
+                className="bg-[#eeeee] w-1/2  rounded px-4 py-2 border  text-lg placeholder:text-base"
                 type="text"
                 placeholder="Last name"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
               />
             </div>
-            <h3 className="text-base font-medium mb-2">What's your email</h3>
+            <h3 className="text-lg font-medium mb-2">What's your email</h3>
             <input
               required
-              className="bg-[#eeeee] mb-6 rounded px-4 py-2 border w-full text-base placeholder:text-sm"
+              className="bg-[#eeeee] mb-6 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
               type="email"
               placeholder="email@example.com"
+              value={email}
+              onChange={(e)=>{
+                setEmail(e.target.value)
+              }}
             />
 
-            <h3 className="text-base font-medium mb-2">Enter Password</h3>
+            <h3 className="text-lg font-medium mb-2">Enter Password</h3>
             <input
               required
-              className="bg-[#eeeee] mb-6 rounded px-4 py-2 border w-full text-base placeholder:text-sm"
+              className="bg-[#eeeee] mb-6 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
               type="password"
               placeholder="password"
+              value={password}
+              onChange={(e)=>{
+                setPassword(e.target.value)
+              }}
             />
 
-            <button className="bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2  w-full text-base placeholder:text-sm">
+            <button className="bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2  w-full text-lg placeholder:text-base">
               Login
             </button>
           </form>
