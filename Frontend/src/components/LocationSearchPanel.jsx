4d6fc7162cc9544e7a04935ca7cffd0dea18f43f
Karansri123 <karansrivastava189@gmailcom>
2024-12-19 22:07:49 +0530

    Search Pannel Ui Done

---
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,37 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const LocationSearchPanel = () => {
+  return (
+    <div>
+        {/* this is just a sample data */}
+        
+        <div className='flex gap-2 my-4 items-center justify-start'>
+            <h2 className='bg-[#eee] h-8 w-12 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill"></i></h2>
+            <h4 className='font-medium'>Polytechnic Chauraha Lucknow, Uttar Pradesh</h4>
+        </div>
+
+         <div className='flex gap-2 my-4 items-center justify-start'>
+            <h2 className='bg-[#eee] h-8 w-12 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill"></i></h2>
+            <h4 className='font-medium'>Polytechnic Chauraha Lucknow, Uttar Pradesh</h4>
+        </div>
+
+        <div className='flex gap-2 my-4 items-center justify-start'>
+            <h2 className='bg-[#eee] h-8 w-12 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill"></i></h2>
+            <h4 className='font-medium'>Polytechnic Chauraha Lucknow, Uttar Pradesh</h4>
+        </div>
+
+        <div className='flex gap-2 my-4 items-center justify-start'>
+            <h2 className='bg-[#eee] h-8 w-12 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill"></i></h2>
+            <h4 className='font-medium'>Polytechnic Chauraha Lucknow, Uttar Pradesh</h4>
+        </div>
+
+        <div className='flex gap-2 my-4 items-center justify-start'>
+            <h2 className='bg-[#eee] h-8 w-12 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill"></i></h2>
+            <h4 className='font-medium'>Polytechnic Chauraha Lucknow, Uttar Pradesh</h4>
+        </div>
+    </div>
+  )
+}
+
+export default LocationSearchPanel
